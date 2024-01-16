0028b4d0e2ea5f082e657451c6d9ba02c02d0653
ChrisAyo <40206164+ChrisAyo@users.noreply.github.com>
2024-01-16 19:28:11 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 const scoreCounter = document.querySelector('.score-counter');
 const grid = document.querySelector('.grid');
 const endGameScreen = document.querySelector('.end-game-screen');
-const endGameText = document.querySelector('.end-game-text')
-const playAgainButton = document.querySelector('play-again')
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('play-again');
 
 const totalCells = 100;
 const totalBombs = 90;
 const maxScore = 5;
-const bombList = [];
+const bombsList = [];
 
 let score = 0;
 
-const cell = document.createElement('div')
-cell.classList.add('cell')
+function updateScore() {
+  score++;
+  scoreCounter.innerText = score.toString().padStart(5, '0');
 
-for (let i = 1; i<=100; i++ ) {
-  const cell = document.createElement('div')
-cell.classList.add('cell')
-
-cell.addEventListener('click',function () {
-console.log()
-})
-
-grid.appendChild(cell);
+  if (score === maxScore) {
+    endGame();
+  }
 }
+
+const cell = document.createElement('div');
+cell.classList.add('cell');
+
+for (let i = 1; i <= 100; i++) {
+  const cell = document.createElement('div');
+  cell.classList.add('cell');
+
+  cell.addEventListener('click', function () {
+    cell.classList.add('cell-clicked');
+    updateScore();
+  });
+
+  grid.appendChild(cell);
+}
+
+while (bombsList.length < totalBombs) {
+  const randomNumber = Math.floor(Math.random() * totalCells) + 1;
+
+  if (!bombsList.includes(randomNumber)) {
+    bombsList.push(randomNumber);
+  }
+  console.log(randomNumber);
+  bombsList.push('Hello');
+}
+
+function endGame() {
+  endGameText.innerHTML = 'YOU<br>WON';
+  endGameScreen.classList.add('win');
+  endGameScreen.classList.remove('hidden');
+}
+
+playAgainButton.addEventListener('click', function ())
